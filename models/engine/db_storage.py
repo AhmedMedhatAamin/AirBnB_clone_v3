4bf8532999e1bc9912034d7bf20eec2dcb73eb18
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2024-04-27 08:07:11 -0700

    ahmed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,7 +21,8 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """Interacts with the MySQL database"""
+
     __engine = None
     __session = None
 
@@ -40,8 +41,41 @@ class DBStorage:
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
+    def get(self, cls, id):
+        """
+        Retrieve one object by its ID.
+
+        Args:
+            cls: Class of the object.
+            id: ID of the object.
+
+        Returns:
+            Object based on the class and its ID, or None if not found.
+        """
+        objects = self.all(cls)
+        for obj_id, obj in objects.items():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """
+        Count the number of objects in storage matching the given class.
+
+        Args:
+            cls: Class of the object (optional).
+
+        Returns:
+            Number of objects in storage matching the given class. If no class is passed,
+            returns the count of all objects in storage.
+        """
+        if cls is None:
+            return len(self.all())
+        else:
+            return sum(1 for obj in self.all().values() if type(obj) == cls)
+
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query all objects or objects of a specific class"""
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -49,28 +83,29 @@ class DBStorage:
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return (new_dict)
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """Reload data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,39 @@ class FileStorage:
             return new_dict
         return self.__objects
 
+    def get(self, cls, id):
+        """
+        Retrieve one object by its ID.
+
+        Args:
+            cls: Class of the object.
+            id: ID of the object.
+
+        Returns:
+            Object based on the class and its ID, or None if not found.
+        """
+        objects = self.all(cls)
+        for obj_id, obj in objects.items():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """
+        Count the number of objects in storage matching the given class.
+
+        Args:
+            cls: Class of the object (optional).
+
+        Returns:
+            Number of objects in storage matching the given class. If no class is passed,
+            returns the count of all objects in storage.
+        """
+        if cls is None:
+            return len(self.__objects)
+        else:
+            return sum(1 for obj in self.__objects.values() if type(obj) == cls)
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:

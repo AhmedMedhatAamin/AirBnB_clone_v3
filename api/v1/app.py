0191e11a47c1e017210e0a25c29d24862e2c08f8
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2024-04-29 12:04:18 -0700

    ahmed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,36 @@
-from flask import Flask, jsonify
-from app import storage
+#!/usr/bin/python3
+
+'''api status'''
+from flask import jsonify
+from models import storage
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 from api.v1.views import app_views
-import os
 
-app = Flask(__name__)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def return_status():
+    '''return status'''
+    return jsonify(status='OK')
 
-app.register_blueprint(app_views)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def return_stats():
+    '''return stats'''
+    model_counts = {
+        'states': storage.count(State),
+        'users': storage.count(User),
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review)
+    }
+    return jsonify(model_counts), 200
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
+@app_views.errorhandler(404)
+def not_found(error):
+    '''handle 404 errors'''
+    return jsonify(error='Not found'), 404
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
